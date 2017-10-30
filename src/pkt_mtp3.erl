89e2fa8df4cf1151835425797c5b21f472bbe810
Andrzej Trawiński <andrzej.trawinski@jtendo.com>
2017-10-30 15:07:50 +0100

    Fix decoding of MTP3 Routing Label

--- src/pkt_mtp3.erl
+++ src/pkt_mtp3.erl
@@ -4,7 +4,9 @@
 
 -export([codec/1]).
 
-codec(<<NI:2, _Spare:2, SI:4, SLS:4, OPC:14, DPC:14, Payload/binary>>) ->
+codec(<<NI:2, _Spare:2, SI:4, RL:32/integer-little, Payload/binary>>) ->
+    <<SLS:4, OPC:14, DPC:14>> = <<RL:32/integer-big>>,
     {#mtp3{network_ind = NI, service_ind = SI, routing_label = #mtp3_routing_label{sls = SLS, opc = OPC, dpc = DPC}}, Payload};
 codec(#mtp3{network_ind = NI, service_ind = SI, routing_label = #mtp3_routing_label{sls = SLS, opc = OPC, dpc = DPC}}) ->
-    <<NI:2, 0:2, SI:4, SLS:4, OPC:14, DPC:14>>.
+    <<RL:32/integer-little>> = <<SLS:4, OPC:14, DPC:14>>,
+    <<NI:2, 0:2, SI:4, RL:32/integer-big>>.
